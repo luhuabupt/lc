ecd6060157d1f27b778ca4615544d500cab9cd65
luhua <luhua@xiaomi.com>
2021-11-05 18:50:06 +0800

    add some

--- 2020/november.go
+++ 2020/november.go
@@ -1,1 +1,18 @@
 package main
+
+func longestSubsequence(arr []int, difference int) int {
+	dp := map[int]int{}
+	ans := 1
+	for _, v := range arr {
+		if dp[v-difference] == 0 {
+			dp[v] = 1
+		} else if dp[v-difference] >= dp[v] {
+			dp[v] = dp[v-difference] + 1
+			if dp[v] > ans {
+				ans = dp[v]
+			}
+		}
+	}
+
+	return ans
+}
