9996740601a5e381e2c6bf362c4f86b86c6da102
luhua <luhua@xiaomi.com>
2021-12-02 17:17:41 +0800

    add some

---
+++ 2020/train/december.go
@@ -0,0 +1,8 @@
+package main
+
+func findLongestChain(pairs [][]int) int {
+	dp := [][]int{}
+	for _, v := range pairs {
+
+	}
+}
--- tmp.go
+++ tmp.go
@@ -1,15 +1,79 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"runtime"
+	"time"
+)
 
-type Abc struct {
-	node []*Abc
+// stack represents a stack of program counters.
+type stack []uintptr
+
+type withStack struct {
+	error
+	*stack
+}
+
+type withMessage struct {
+	cause error
+	msg   string
+}
+
+type TaskMsg struct {
+	ActionType int   `json:"actionType"`
+	ActionId   int64 `json:"actionId"`
+	Mid        int64 `json:"mid"`
+	Ts         int64 `json:"date"`
 }
 
 func main() {
-	for j := 100000; j > 0; j -= (j & -j) {
-		fmt.Println(j)
-		fmt.Printf("%#b ", j&-j)
-		fmt.Printf("%#b \n", j)
+	var taskMsg TaskMsg
+	msg := TaskMsg{
+		1,
+		2,
+		122,
+		time.Now().Unix(),
+	}
+	msgStr, _ := json.Marshal(msg)
+	err := json.Unmarshal(msgStr, &taskMsg)
+	fmt.Println(err)
+	fmt.Println(taskMsg)
+}
+
+func (w *withMessage) Error() string { return w.msg + ": " + w.cause.Error() }
+func (w *withMessage) Cause() error  { return w.cause }
+
+func callers() *stack {
+	const depth = 32
+	var pcs [depth]uintptr
+	n := runtime.Callers(3, pcs[:])
+	var st stack = pcs[0:n]
+	return &st
+}
+
+func Wrapf(err error, format string, args ...interface{}) error {
+	if err == nil {
+		return nil
+	}
+	err = &withMessage{
+		cause: err,
+		msg:   fmt.Sprintf(format, args...),
+	}
+	return &withStack{
+		err,
+		callers(),
+	}
+}
+
+// WithMessage annotates err with a new message.
+// If err is nil, WithMessage returns nil.
+func WithMessage(err error, message string) error {
+	if err == nil {
+		return nil
+	}
+	return &withMessage{
+		cause: err,
+		msg:   message,
 	}
 }
