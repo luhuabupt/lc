b7aba808edddda6fcb3c44a19e18dfad5e899fca
luhua <luhua@xiaomi.com>
2022-02-14 10:29:22 +0800

    add

---
+++ 2022/auto/leetcode/editor/cn/540-singleElementInASortedArray.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(singleNonDuplicate([]int{1, 1, 2, 3, 3, 4, 4, 8, 8}))
+	fmt.Println(singleNonDuplicate([]int{1,1,2}))
+	fmt.Println(singleNonDuplicate([]int{3, 3, 7, 7, 10, 11, 11}))
+	fmt.Println(singleNonDuplicate([]int{1,2,2}))
+}
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func singleNonDuplicate(a []int) int {
+	n := len(a)
+	l, r, m := 0, n-1, 0
+	for l <= r {
+		m = (l + r) / 2
+		if (m == 0 || a[m] != a[m-1]) && (m == n-1 || a[m] != a[m+1]) {
+			break
+		}
+
+		x := m - 1
+		if m > 0 && a[m-1] == a[m] {
+			x = m - 2
+		}
+
+		if x&1 == 0 {
+			r = x
+		} else {
+			l = x + 3
+		}
+	}
+
+	return a[m]
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
