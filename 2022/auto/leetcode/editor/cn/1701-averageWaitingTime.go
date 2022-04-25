5e4f52342715ad3d9497ddd1692b56f547cd33e8
luhua <luhua@xiaomi.com>
2022-04-25 15:41:12 +0800

    merge

---
+++ 2022/auto/leetcode/editor/cn/1701-averageWaitingTime.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	
+}
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func averageWaitingTime(customers [][]int) float64 {
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
